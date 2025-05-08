bd415c70a4173e99495f7c1716f0806042448be1
goncalofgalmeida <102439759+goncalofgalmeida@users.noreply.github.com>
2025-05-08 16:17:51 +0100

    Create basic Asteroids game
    - Player movement using AWSD
    - Ability to shoot against asteroids
    - Asteroids split or disappear, depending on size

---
+++ asteroid.py
@@ -0,0 +1,27 @@
+import random
+from constants import *
+from circleshape import *
+
+class Asteroid(CircleShape):
+	def __init__(self, x, y, radius):
+		super().__init__(x, y, radius)
+
+	def draw(self, screen):
+		pygame.draw.circle(screen, "white", self.position, self.radius)
+
+	def split(self):
+		self.kill()
+		if self.radius <= ASTEROID_MIN_RADIUS:
+			return
+		random_angle = random.uniform(20, 50)
+		new_vector_1 = self.velocity.rotate(random_angle)
+		new_vector_2 = self.velocity.rotate(-random_angle)
+		new_radius = self.radius - ASTEROID_MIN_RADIUS
+		new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+		new_asteroid_1.velocity = new_vector_1 * 1.2
+		new_asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+		new_asteroid_2.velocity = new_vector_2 * 1.2
+
+	
+	def update(self, dt):
+		self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
+import sys
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
 	pygame.init()
@@ -7,12 +11,38 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = (updatable)
+	Shot.containers = (shots, updatable, drawable)
+	player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroid_field_1 = AsteroidField()
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
+		updatable.update(dt)
+		for asteroid in asteroids:
+			if asteroid.check_colision(player_1):
+				print("Game over!")
+				sys.exit()
+		for asteroid in asteroids:
+			for shot in shots:
+				if asteroid.check_colision(shot):
+					shot.kill()
+					asteroid.split()
+		for obj in drawable:
+			obj.draw(screen)
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
